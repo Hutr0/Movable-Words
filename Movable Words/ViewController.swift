563eecc02d1ac42b14e137af7e9fa43818794842
Hutr0 <Hutro-games@yandex.ru>
2020-09-28 10:15:34 +0300

    Added new button and bugs fixed

--- Movable Words/ViewController.swift
+++ Movable Words/ViewController.swift
@@ -9,21 +9,17 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var wordsTF: UITextField!
+    @IBOutlet weak var fromTF: UITextField!
+    @IBOutlet weak var toTF: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
-    
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func toMoveWords(_ sender: Any) {
     }
-    */
-
+    
 }
